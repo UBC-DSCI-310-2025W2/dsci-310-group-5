cdce1931d81375e81bc2d18b030d2acdf8036090
snowy17037 <171077626+snowy17037@users.noreply.github.com>
2026-03-20 21:21:11 -0700

    Merged scripts from scripts branch

---
+++ scripts/02-clean_data.R
@@ -0,0 +1,35 @@
+"Usage:
+  02-clean_data.R --input=<path> --output=<path> --output_results=<path>
+
+Options:
+  --input=<path>             Path to raw data
+  --output=<path>           Path to save cleaned data (clean_bechdel.csv)
+  --output_results=<path>    Path to save tables (table1, table2, table3)
+" -> doc
+
+library(docopt)
+library(dplyr)
+
+main <- function(input, output, output_results) {
+  bechdel <- read.csv(input)
+  movie_data <- bechdel[, c("budget", "domgross")]
+  movie_data <- movie_data %>% mutate(budget = as.numeric(budget), domgross = as.numeric(domgross))
+
+  dir.create(output_results, recursive = TRUE, showWarnings = FALSE)
+  dir.create(dirname(output), recursive = TRUE, showWarnings = FALSE)
+
+  write.csv(head(movie_data, 6), paste0(output_results, "/table1_first_six_rows.csv"), row.names = FALSE)
+
+  movie_data <- movie_data %>%
+    filter(!is.na(budget), !is.na(domgross))
+
+  write.csv(movie_data[movie_data$domgross == 0, ], paste0(output_results, "/table2_zero_revenue.csv"), row.names = FALSE)
+
+  movie_data <- movie_data %>%
+    filter(budget > 0, domgross > 0)
+
+  write.csv(movie_data, output, row.names = FALSE)
+}
+
+opt <- docopt(doc)
+main(opt$input, opt$output, opt$output_results)
